249b260ba2b7287c4905a9d284c0c6abdb67f2e2
Vishal Badhan <135725438+VishalBadhan02@users.noreply.github.com>
2024-07-13 23:28:09 +0530

    mongoose

--- backend/config/index.js
+++ backend/config/index.js
@@ -2,7 +2,7 @@ const Config = {
     "HOST": "127.0.0.1",
     "PORT": 5050,
     "DATABASE": {
-        URL: "mongodb://127.0.0.1:27017/Playpal"
+        URL: "mongodb+srv://vishalbadhan81:NZRcV3Bw7EUePTDy@cluster0.8vfuck9.mongodb.net/"
     },
     "JWTSECRETKEY": "VISHAL"
 }
